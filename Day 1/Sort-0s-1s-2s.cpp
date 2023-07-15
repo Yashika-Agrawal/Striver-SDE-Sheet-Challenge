c633a73cea4f5dd92bb2194be3c8d1fc91adbd5d
Yashika Agrawal <yashikayash2002@gmail.com>
2023-07-16 01:49:50 +0530

    Create Sort-0s-1s-2s.cpp

---
+++ Day 1/Sort-0s-1s-2s.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+void sort012(int *a, int n)
+{
+   int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high)
+        {
+            if(a[mid]==0)
+            {
+                swap(a[mid],a[low]);
+                low++;
+                mid++;
+            }
+             else if(a[mid]==1)
+            {
+                mid++;
+            }
+            else 
+            {
+                swap(a[mid],a[high]);
+                high--;
+            }
+        }
+}
